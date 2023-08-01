8d8186ccf6f37613942a3cbfc2452ed1a70dbfa1
coldstar-507 <scott.1212@gmx.com>
2023-08-01 17:28:10 -0400

    server config update

--- server/server.go
+++ server/server.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"time"
 
 	"log"
 
@@ -10,6 +13,7 @@ import (
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/db"
 	"firebase.google.com/go/v4/messaging"
+	"golang.org/x/oauth2/google"
 )
 
 type serverShard struct {
@@ -24,16 +28,22 @@ const (
 )
 
 type server struct {
-	Shards    map[string]([nShard]serverShard)
-	Messager  *messaging.Client
-	Firestore *firestore.Client
+	Shards     map[string]([nShard]serverShard)
+	Messager   *messaging.Client
+	Firestore  *firestore.Client
+	SignedOpts *storage.SignedURLOptions
 }
 
 var Client *server
 
 func ServerInit(ctx context.Context) {
 
-	// opt := option.WithCredentialsFile("C:/Users/coton/Documents/project-down4/service-accounts/down4-26ee1-8433e5b5e7d2.json")
+	p := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	saKey, err := ioutil.ReadFile(p)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	cfg, err := google.JWTConfigFromJSON(saKey)
 
 	app, err := firebase.NewApp(ctx, &firebase.Config{})
 	if err != nil {
@@ -45,14 +55,21 @@ func ServerInit(ctx context.Context) {
 		log.Fatalf("error initializing messager: %v\n", err)
 	}
 
-	fs, err := firestore.NewClient(ctx, "down4-26ee1")
+	fs, err := app.Firestore(ctx)
 	if err != nil {
 		log.Fatalf("error initializing db: %v\n", err)
 	}
 
-	stor, err := storage.NewClient(ctx)
+	stor, err := app.Storage(ctx)
 	if err != nil {
 		log.Fatalf("error initializing storage: %v\n", err)
+	}
+
+	sUrls := &storage.SignedURLOptions{
+		GoogleAccessID: cfg.Email,
+		PrivateKey:     cfg.PrivateKey,
+		Method:         "GET",
+		Expires:        time.Now().Add(time.Hour * 24 * 4),
 	}
 
 	db_am_1, _ := app.DatabaseWithURL(ctx, "https://down4-26ee1-fd90e-us1.firebaseio.com/")
@@ -62,22 +79,24 @@ func ServerInit(ctx context.Context) {
 	db_as_1, _ := app.DatabaseWithURL(ctx, "https://down4-26ee1-8511f-sea1.asia-southeast1.firebasedatabase.app/")
 	db_as_2, _ := app.DatabaseWithURL(ctx, "https://down4-26ee1-d98a8-sea2.asia-southeast1.firebasedatabase.app/")
 
-	tmp_am_1 := stor.Bucket("down4-26ee1-us1-tmp")
-	tmp_am_2 := stor.Bucket("down4-26ee1-us2-tmp")
-	tmp_eu_1 := stor.Bucket("down4-26ee1-eu1-tmp")
-	tmp_eu_2 := stor.Bucket("down4-26ee1-eu2-tmp")
-	tmp_as_1 := stor.Bucket("down4-26ee1-sea1-tmp")
-	tmp_as_2 := stor.Bucket("down4-26ee1-sea2-tmp")
+	tmp_am_1, _ := stor.Bucket("down4-26ee1-us1-tmp")
+	tmp_am_2, _ := stor.Bucket("down4-26ee1-us2-tmp")
+	tmp_eu_1, _ := stor.Bucket("down4-26ee1-eu1-tmp")
+	tmp_eu_2, _ := stor.Bucket("down4-26ee1-eu2-tmp")
+	tmp_as_1, _ := stor.Bucket("down4-26ee1-sea1-tmp")
+	tmp_as_2, _ := stor.Bucket("down4-26ee1-sea2-tmp")
 
-	st_am_1 := stor.Bucket("down4-26ee1-us1")
-	st_am_2 := stor.Bucket("down4-26ee1-us2")
-	st_eu_1 := stor.Bucket("down4-26ee1-eu1")
-	st_eu_2 := stor.Bucket("down4-26ee1-eu2")
-	st_as_1 := stor.Bucket("down4-26ee1-sea1")
-	st_as_2 := stor.Bucket("down4-26ee1-sea2")
+	st_am_1, _ := stor.Bucket("down4-26ee1-us1")
+	st_am_2, _ := stor.Bucket("down4-26ee1-us2")
+	st_eu_1, _ := stor.Bucket("down4-26ee1-eu1")
+	st_eu_2, _ := stor.Bucket("down4-26ee1-eu2")
+	st_as_1, _ := stor.Bucket("down4-26ee1-sea1")
+	st_as_2, _ := stor.Bucket("down4-26ee1-sea2")
 
 	Client = &server{
-		Firestore: fs,
+		SignedOpts: sUrls,
+		Firestore:  fs,
+		Messager:   msgr,
 		Shards: map[string][nShard]serverShard{
 			"america": {
 				serverShard{
@@ -116,6 +135,5 @@ func ServerInit(ctx context.Context) {
 				},
 			},
 		},
-		Messager: msgr,
 	}
 }
